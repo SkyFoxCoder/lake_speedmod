7e70ac912d16813b1b9ccd1fc54ea4479e64f86d
SkyFoxCoder <skyfoxcoder@gmail.com>
2022-07-10 15:22:16 -0400

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx;
+using EVP;
+using HarmonyLib;
+
+namespace Lake_SpeedMod
+{
+    [BepInPlugin("com.skyfoxcoder.lake_speedmod", "Lake Speed Mod", "1.0")]
+    [BepInProcess("Lake.exe")]
+    public class Lake_SpeedMod : BaseUnityPlugin
+    {
+        private static Harmony _harmonyInstance;
+
+        void Awake()
+        {
+            _harmonyInstance = new Harmony("com.skyfoxcoder.lake_speedmod");
+            _harmonyInstance.PatchAll();
+        }
+
+        void OnDestroy()
+        {
+            _harmonyInstance.UnpatchSelf();
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerMovementController), "Awake")]
+    class PlayerPatch
+    {
+        private static readonly AccessTools.FieldRef<PlayerMovementController, float> sprintSpeedModifierRef = AccessTools.FieldRefAccess<PlayerMovementController, float>("SprintSpeedModifier");
+        static void Prefix(PlayerMovementController __instance)
+        {
+            sprintSpeedModifierRef(__instance) = 5f;
+        }
+    }
+
+    [HarmonyPatch(typeof(VehicleController), "OnEnable")]
+    class VehiclePatch
+    {
+        private static readonly AccessTools.FieldRef<VehicleController, float> forceCurveShapeRef = AccessTools.FieldRefAccess<VehicleController, float>("forceCurveShape");
+        private static readonly AccessTools.FieldRef<VehicleController, float> driveForceToMaxSlipRef = AccessTools.FieldRefAccess<VehicleController, float>("driveForceToMaxSlip");
+        private static readonly AccessTools.FieldRef<VehicleController, float> maxSpeedForwardRef = AccessTools.FieldRefAccess<VehicleController, float>("maxSpeedForward");
+        private static readonly AccessTools.FieldRef<VehicleController, float> antiRollRef = AccessTools.FieldRefAccess<VehicleController, float>("antiRoll");
+        private static readonly AccessTools.FieldRef<VehicleController, float> maxDriveForceRef = AccessTools.FieldRefAccess<VehicleController, float>("maxDriveForce");
+        private static readonly AccessTools.FieldRef<VehicleController, float> tireFrictionRef = AccessTools.FieldRefAccess<VehicleController, float>("tireFriction");
+        private static readonly AccessTools.FieldRef<VehicleController, float> aeroBalanceRef = AccessTools.FieldRefAccess<VehicleController, float>("aeroBalance");
+
+        static void Prefix(VehicleController __instance)
+        {
+            forceCurveShapeRef(__instance) = 0.9999f;
+            driveForceToMaxSlipRef(__instance) = 0;
+            maxSpeedForwardRef(__instance) = 40f;
+            antiRollRef(__instance) = 1f;
+            maxDriveForceRef(__instance) = 10000f;
+            tireFrictionRef(__instance) = 3f;
+            aeroBalanceRef(__instance) = 100;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# Lake (the game) - Speed Mod
+
+> See what it does: https://www.youtube.com/watch?v=xQD7RP2kzxc
+
+## Prerequisite:
+- BepInEx - v5.4.19+
+
+## Installation :
+Download the latest release build, extract the `SpeedMod.dll` file into the game's directory, under `BepInEx/plugins`.
+
+The mod will be `automatically enabled` ingame. \
+To use it, `Hold` the `LeftShift` key, either on foot, or in the vehicle.
+
+# Compiling
+
+## Build dependencies :
+
+The following dlls are going in the project's `/libs` folder, get them from GitHub :
+- `BepInEx.dll` - v5.4.19+
+- `BepInEx.Harmony.dll` - v2+ ( Comes with BepInEx )
+
+The next dll is available as a `locally installed` (GitHub) .nuget extension, not on the store.
+- `0Harmony.dll` ( HarmonyX - v2.10+ )
+
+Game-specifics dlls ( Put them in `/libs` ) :
+- `Assembly-CSharp.dll`
+- `UnityEngine.dll`
+- `UnityEngine.CoreModule.dll`
